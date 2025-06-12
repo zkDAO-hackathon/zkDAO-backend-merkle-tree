70df484d93b0c1fd79d15c4056777cf92fbe27d4
salviega <salviega6@gmail.com>
2025-06-12 17:22:59 -0500

    🚧 Working in the transformation request

--- src/modules/usecases/merkle-tree/merkle-tree.controller.ts
+++ src/modules/usecases/merkle-tree/merkle-tree.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
+
+import { MerkleTreeService } from './merkle-tree.service'
 
 @Controller('merkle-tree')
-export class MerkleTreeController {}
+export class MerkleTreeController {
+	constructor(private readonly merkleTreeService: MerkleTreeService) {}
+
+	// TODO: Add Interceptor and DTO validation
+	@Post()
+	getMerkleTree(@Body() body): { data: string } {
+		const { proposals } = body
+		return { data: this.merkleTreeService.generateMerkleTree(proposals) }
+	}
+}
--- src/modules/usecases/merkle-tree/merkle-tree.service.ts
+++ src/modules/usecases/merkle-tree/merkle-tree.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class MerkleTreeService {}
+export class MerkleTreeService {
+	generateMerkleTree(proposals: string[]): string {
+		return `Merkle Tree generated with proposals: ${proposals.join(', ')}`
+	}
+}
